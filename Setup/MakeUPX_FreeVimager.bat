769b39617a46333a5b2ddecf49e39f4967e1cf59
Oliver Pfister <oliver.pfister@contaware.com>
2010-02-20 14:20:18 +0000

    Do not compress FreeVimager icons with UPX, otherwise the file association icons are not shown!
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@98 15f0fa91-3113-114a-a8b1-9261d5763783

--- Setup/MakeUPX_FreeVimager.bat
+++ Setup/MakeUPX_FreeVimager.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Set --compress-exports=0 to be able to run it under Wine
 copy ..\bin\FreeVimager\FreeVimager.exe .\FreeVimager.exe
-upx --ultra-brute --8mb-ram FreeVimager.exe
+upx --compress-icons=0 --ultra-brute --8mb-ram FreeVimager.exe
 pause
--- Setup/MakeUPX_FreeVimagerw.bat
+++ Setup/MakeUPX_FreeVimagerw.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Set --compress-exports=0 to be able to run it under Wine
 copy ..\bin\FreeVimagerw\FreeVimagerw.exe .\FreeVimagerw.exe
-upx --ultra-brute --8mb-ram FreeVimagerw.exe
+upx --compress-icons=0 --ultra-brute --8mb-ram FreeVimagerw.exe
 pause
