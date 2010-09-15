7c345384e98af471d035cb49dd64e1f6007605c3
Oliver Pfister <oliver.pfister@contaware.com>
2010-09-15 10:24:38 +0000

    Updated to libpng-1.2.44
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@200 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -33,7 +33,7 @@
 #define APPMUTEXNAME									_T("FreeVimagerAppMutex")
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #endif
-#define APPVERSION										_T("2.0.9beta1")
+#define APPVERSION										_T("2.0.9")
 #define SLIDESHOWNAME									_T("Start.exe")
 #define SLIDESHOWSETTINGSNAME							_T("Settings.xml")
 #define NEROBURNNAME									_T("NeroBurn.exe")
