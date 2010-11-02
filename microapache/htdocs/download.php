bddeb1b6b941ead151e75b99fdf2cb3eda68918d
Oliver Pfister <oliver.pfister@contaware.com>
2010-11-02 09:19:02 +0000

    Added missing copyright header
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@214 15f0fa91-3113-114a-a8b1-9261d5763783

--- microapache/htdocs/download.php
+++ microapache/htdocs/download.php
@@ -1,4 +1,18 @@
 <?php
+/* Copyright © 2005-2010 Oliver Pfister, Switzerland, Web: www.contaware.com
+ *
+ * This source code is provided "as is", without any express or implied 
+ * warranty. In no event will the author/developer be held liable or 
+ * accountable in any way for any damages arising from the use of this 
+ * source code. 
+ *
+ * Copyright / Usage Details: 
+ *
+ * You are allowed to modify this source code in any way you want provided 
+ * that this license statement remains with this source code and that you
+ * put a comment in this source code about the changes you have done.
+ *
+ *-------------------------------------------------------------------------*/
 $filename = $_GET['file'];
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
 if ($doc_root == "")
