d1271c5db9dcb1a75264d2217534bab4a027d851
Oliver Pfister <oliver.pfister@contaware.com>
2011-05-27 08:19:42 +0000

    Added Czech and Slovak web interface languages
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@307 15f0fa91-3113-114a-a8b1-9261d5763783

---
+++ microapache/htdocs/languages/czech.php
@@ -0,0 +1,43 @@
+﻿<?php
+/* Copyright © 2005-2011 Oliver Pfister, Switzerland, Web: www.contaware.com
+ *
+ * This source code is provided "as is", without any express or implied 
+ * warranty. In no event will the author/developer be held liable or 
+ * accountable in any way for any damages arising from the use of this 
+ * source code. 
+ *
+ * Copyright / Usage Details: 
+ *
+ * You are allowed to modify this source code in any way you want provided 
+ * that this license statement remains with this source code and that you
+ * put a comment in this source code about the changes you have done.
+ *
+ *-------------------------------------------------------------------------*/
+define("TODAY","Dnes");
+define("SHOWCALENDAR","Kalendář");
+define("DAYHEADERS","'N','P','U','S','Š','P','S'"); // Starting from sunday!
+define("DAYNAMES","'Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'"); // Starting from sunday!
+define("MONDAYSTARTSWEEK","1");	// 1: monday starts the week, 0: sunday starts the week
+define("SHORTMONTHNAMES","'Led','Ún','Bře','Dub','Kvě','Červ','Čerc','Srp','Zář','Říj','Lis','Pro'");
+define("MONTHNAMES","'Leden','Únor','Březen','Duben','Květen','Červen','Červenec','Srpen','Září','Říjen','Listopad','Prosinec'");
+define("YEAR","rok");
+define("MONTH","měsíc");
+define("IN","za");
+define("DAY","den");
+define("DAYS","dny");
+define("DAYAGO","den před");
+define("DAYSAGO","dny před");
+define("NOFILE","Žádné soubory");
+define("NOFILESFOR","Žádné soubory z");
+define("PAGES","stránky");
+define("BACK","Zpět");
+define("LOCALTIME","Aktuální čas");
+define("VIEWSNAPSHOTSFOR","Prohlédnout snapshoty");
+define("VIEWFILESFOR","Prohlédnout soubory");
+define("SELECT","vybrat");
+define("IMAGERELOADIN","Obraz bude obnoven za");
+define("SECONDS","sekund");
+define("RELOAD","Obnovit");
+define("HOME","Vybrat kameru");
+define("PRINTCOMMAND","Vytisknout");
+?>
---
+++ microapache/htdocs/languages/slovak.php
@@ -0,0 +1,43 @@
+﻿<?php
+/* Copyright © 2005-2011 Oliver Pfister, Switzerland, Web: www.contaware.com
+ *
+ * This source code is provided "as is", without any express or implied 
+ * warranty. In no event will the author/developer be held liable or 
+ * accountable in any way for any damages arising from the use of this 
+ * source code. 
+ *
+ * Copyright / Usage Details: 
+ *
+ * You are allowed to modify this source code in any way you want provided 
+ * that this license statement remains with this source code and that you
+ * put a comment in this source code about the changes you have done.
+ *
+ *-------------------------------------------------------------------------*/
+define("TODAY","Dnes");
+define("SHOWCALENDAR","Kalendár");
+define("DAYHEADERS","'N','P','U','S','Š','P','S'"); // Starting from sunday!
+define("DAYNAMES","'Nedeľa','Pondelok','Utorok','Streda','Štvrtok','Piatok','Sobota'"); // Starting from sunday!
+define("MONDAYSTARTSWEEK","1");	// 1: monday starts the week, 0: sunday starts the week
+define("SHORTMONTHNAMES","'Jan','Feb','Mar','Apr','Máj','Jún','Júl','Aug','Sep','Okt','Nov','Dec'");
+define("MONTHNAMES","'Január','Február','Marec','Apríl','Máj','Jún','Júl','August','September','Október','November','December'");
+define("YEAR","rok");
+define("MONTH","mesiac");
+define("IN","o");
+define("DAY","deň");
+define("DAYS","dni");
+define("DAYAGO","deň dozadu");
+define("DAYSAGO","dni dozadu");
+define("NOFILE","Žiadne súbory");
+define("NOFILESFOR","Žiadne súbory z");
+define("PAGES","strany");
+define("BACK","Späť");
+define("LOCALTIME","Aktuálny čas");
+define("VIEWSNAPSHOTSFOR","Prezrieť snapshoty");
+define("VIEWFILESFOR","Prezrieť súbory");
+define("SELECT","vybrať");
+define("IMAGERELOADIN","Obraz bude obnovený za");
+define("SECONDS","sekúnd");
+define("RELOAD","Obnoviť");
+define("HOME","Vybrať kameru");
+define("PRINTCOMMAND","Vytlačiť");
+?>
