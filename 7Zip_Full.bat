4484fe53dc62000b99937da2d72fc10b433d2101
Oliver Pfister <oliver.pfister@contaware.com>
2011-09-13 14:50:55 +0000

    Final release 3.5.0
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@361 15f0fa91-3113-114a-a8b1-9261d5763783

---
+++ 7Zip_Full.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Configure 7-Zip installation directory
+set exedir=C:\Program Files\7-Zip
+
+REM Delete old version
+del .\uimager_Full.7z
+
+REM 7-Zip it
+"%exedir%\7z.exe" a -r -mx9 -t7z uimager_Full.7z * -x!uimager_NoTutorials.7z -x!uimager_OnlyTutorials.7z
+
+pause
