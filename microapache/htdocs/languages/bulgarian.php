47c3f6f5922c860acf667e9af3d4c3bba8822b47
Oliver Pfister <oliver.pfister@contaware.com>
2012-11-29 10:35:00 +0000

    Added Bulgarian web interface language
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@668 15f0fa91-3113-114a-a8b1-9261d5763783

---
+++ microapache/htdocs/languages/bulgarian.php
@@ -0,0 +1,28 @@
+﻿<?php
+define("TODAY","днес");
+define("SHOWCALENDAR","Покажи календар");
+define("DAYHEADERS","'Н','П','В','С','Ч','П','С'"); // Starting from sunday!
+define("DAYNAMES","'Неделя','Понеделник','Вторник','Сряда','Четвъртък','Петък','Събота'"); // Starting from sunday!
+define("MONDAYSTARTSWEEK","1");	// 1: monday starts the week, 0: sunday starts the week
+define("SHORTMONTHNAMES","'Ян','Фев','Мар','Апр','Май','Юни','Юли','Авг','Сеп','Окт','Ное','Дек'");
+define("MONTHNAMES","'Януари','Февруари','Март','Април','Май','Юни','Юли','Август','Септември','Октомври','Ноември','Декември'");
+define("YEAR","година");
+define("MONTH","месец");
+define("IN","след");
+define("DAY","ден");
+define("DAYS","дни");
+define("DAYAGO","ден назад");
+define("DAYSAGO","дни назад");
+define("NOFILE","Липсва файл");
+define("NOFILESFOR","Липсват файлове за");
+define("PAGES","страници");
+define("BACK","Назад");
+define("LOCALTIME","Местно време");
+define("VIEWSNAPSHOTSFOR","Преглед на снимка(и) за");
+define("VIEWFILESFOR","Преглед на файл(ове) за");
+define("SELECT","избери");
+define("IMAGERELOADIN","Презареждане на снимката след");
+define("SECONDS","секунди");
+define("RELOAD","Обновяване");
+define("HOME","Избeри камера");
+define("PRINTCOMMAND","печат");
