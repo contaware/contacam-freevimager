08073f6d17a5aeb224c37053a7c686ff125171c5
Oliver Pfister <oliver.pfister@contaware.com>
2014-03-24 10:49:56 +0000

    Final release 4.9.5
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@953 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -29,7 +29,7 @@
 #define APPMUTEXNAME									_T("FreeVimagerAppMutex")
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #endif
-#define APPVERSION										_T("4.9.5beta21")
+#define APPVERSION										_T("4.9.5")
 #define SLIDESHOWNAME									_T("Start.exe")
 
 // Company
