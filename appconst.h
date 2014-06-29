804a03411a9dea563f1e449fa37d78752651cb5c
Oliver Pfister <oliver.pfister@contaware.com>
2014-06-29 20:52:38 +0000

    Updated all ContaCam Tutorials (renamed Device Settings to Camera Advanced Settings)
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1057 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -29,7 +29,7 @@
 #define APPMUTEXNAME									_T("FreeVimagerAppMutex")
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #endif
-#define APPVERSION										_T("4.9.7beta5")
+#define APPVERSION										_T("4.9.7beta6")
 #define SLIDESHOWNAME									_T("Start.exe")
 
 // Company
