da6de276733545b9ad65a3bd082b5d919f6b8580
Oliver Pfister <oliver.pfister@contaware.com>
2014-08-20 21:03:41 +0000

    Added Polish web interface language
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1084 15f0fa91-3113-114a-a8b1-9261d5763783

---
+++ microapache/htdocs/languages/polish.php
@@ -0,0 +1,28 @@
+﻿<?php
+define("TODAY","dzisiaj");
+define("SHOWCALENDAR","Pokaż Kalendarz");
+define("DAYHEADERS","'N','Pn','W','Ś','C','Pt','S'"); // Starting from sunday!
+define("DAYNAMES","'Niedziela','Poniedziałek','Wtorek','Środa','Czwartek','Piątek','Sobota'"); // Starting from sunday!
+define("MONDAYSTARTSWEEK","1");	// 1: monday starts the week, 0: sunday starts the week
+define("SHORTMONTHNAMES","'Sty','Lut','Mar','Kwi','Maj','Cze','Lip','Sie','Wrz','Paź','Lis','Gru'");
+define("MONTHNAMES","'Styczeń','Luty','Marzec','Kiecień','Maj','Czerwiec','Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'");
+define("YEAR","rok");
+define("MONTH","miesiąc");
+define("IN","w");
+define("DAY","dniu");
+define("DAYS","dniach");
+define("DAYAGO","dzień temu");
+define("DAYSAGO","dni temu");
+define("NOFILE","Brak pliku");
+define("NOFILESFOR","Brak plików do");
+define("PAGES","strony");
+define("BACK","Powrót");
+define("LOCALTIME","Czas Lokalny");
+define("VIEWSNAPSHOTSFOR","Pokaż zdjęcia dla");
+define("VIEWFILESFOR","Pokaż pliki dla");
+define("SELECT","wybierz");
+define("IMAGERELOADIN","Zdjęcie załaduje się w");
+define("SECONDS","sekund");
+define("RELOAD","Odswież");
+define("HOME","Wybierz Kamerę");
+define("PRINTCOMMAND","Drukuj");
