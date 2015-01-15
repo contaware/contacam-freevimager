5c1540c3393eeb141d7990eccb8cbe0aa5c60c13
Oliver Pfister <oliver.pfister@contaware.com>
2015-01-15 22:25:46 +0000

    5.0.0 beta2 including latest ffmpeg version built with stack re-alignment option enabled
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1168 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -29,7 +29,7 @@
 #define APPMUTEXNAME									_T("FreeVimagerAppMutex")
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #endif
-#define APPVERSION										_T("5.0.0beta1")
+#define APPVERSION										_T("5.0.0beta2")
 #define SLIDESHOWNAME									_T("Start.exe")
 
 // Company
