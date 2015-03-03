e8ebac5141940a30112433b30e6c60de95490c23
Oliver Pfister <oliver.pfister@contaware.com>
2015-03-03 13:54:12 +0000

    Redirect summaryiframe.php to summarysnapshot.php
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1229 15f0fa91-3113-114a-a8b1-9261d5763783

--- microapache/htdocs/index.php
+++ microapache/htdocs/index.php
@@ -4,5 +4,5 @@ $host     = (!empty($_SERVER['HTTP_X_FORWARDED_HOST'])) ? $_SERVER['HTTP_X_FORWA
 $rel_path = dirname($_SERVER['PHP_SELF']);
 $rel_path = str_replace("\\", "/", $rel_path);
 $rel_path = rtrim($rel_path, "/");
-$file  = DEFAULTPAGE;
+$file     = DEFAULTPAGE;
 header("Location: $scheme://$host$rel_path/$file");
