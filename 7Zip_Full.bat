6d666488e2c5a8ebf2eb123f7bc6db26d091ed82
Oliver Pfister <oliver.pfister@contaware.com>
2015-04-18 21:54:42 +0000

    Exclude in root folder: uImagervs2010.sdf, ?svn\, bin\, ipch\ and obj\
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1270 15f0fa91-3113-114a-a8b1-9261d5763783

--- 7Zip_Full.bat
+++ 7Zip_Full.bat
@@ -7,4 +7,4 @@ REM Delete old version
 del .\uimager_Full.7z
 
 REM 7-Zip it
-"%exedir%\7z.exe" a -r -mx9 -t7z uimager_Full.7z * -xr!?svn\
+"%exedir%\7z.exe" a -r -mx9 -t7z uimager_Full.7z * -xr-!uImagervs2010.sdf -xr-!?svn\ -xr-!bin\ -xr-!ipch\ -xr-!obj\
