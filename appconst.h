eae5a632566d89d887f0f821e0758ec336e27cb6
Oliver Pfister <oliver.pfister@contaware.com>
2015-06-15 21:35:26 +0000

    Released beta4 of ContaCam 5.0.1
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1341 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -31,7 +31,7 @@
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #define MANUAL_ONLINE_PAGE								_T("http://www.contaware.com/freevimagermanual/")
 #endif
-#define APPVERSION										_T("5.0.1beta3")
+#define APPVERSION										_T("5.0.1beta4")
 #define SLIDESHOWNAME									_T("Start.exe")
 
 // Company
