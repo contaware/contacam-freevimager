04d1281da74911cb13dbb37a3ef0eca0c10474da
Oliver Pfister <oliver.pfister@contaware.com>
2015-11-18 09:34:56 +0000

    When deleting do not show error if output file is not existing
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1446 15f0fa91-3113-114a-a8b1-9261d5763783

--- 7Zip_Full.bat
+++ 7Zip_Full.bat
@@ -3,8 +3,8 @@
 REM Configure 7-Zip installation directory
 set exedir=C:\Program Files\7-Zip
 
-REM Delete old version
-del .\uimager_Full.7z
+REM Delete old version if existing
+del .\uimager_Full.7z >nul 2>&1
 
 REM 7-Zip it
 "%exedir%\7z.exe" a -r -mx9 -t7z uimager_Full.7z * -xr-!uImagervs2010.sdf -xr-!?svn\ -xr-!bin\ -xr-!ipch\ -xr-!obj\
