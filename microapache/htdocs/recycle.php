3baaf2b76b053c308b28ef3e3a55f5dd2f2efff1
Oliver Pfister <oliver.pfister@contaware.com>
2016-04-25 21:31:53 +0000

    Updated recycle.php to work with both thumb and non-thumb filenames
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1530 15f0fa91-3113-114a-a8b1-9261d5763783

--- microapache/htdocs/recycle.php
+++ microapache/htdocs/recycle.php
@@ -35,18 +35,21 @@ if (isset($_GET['year']) && isset($_GET['month']) && isset($_GET['day']) && isse
 	
 	// Full path without extension
 	$doc_root = $_SERVER['DOCUMENT_ROOT'];
-	$filenoext =        $_GET['year']."/".$_GET['month']."/".$_GET['day']."/".$_GET['filenamenoext'];
-	$filenoextnothumb = $_GET['year']."/".$_GET['month']."/".$_GET['day']."/".str_replace("_thumb", "", $_GET['filenamenoext']);
+	$filenoext = $_GET['year']."/".$_GET['month']."/".$_GET['day']."/".$_GET['filenamenoext'];
+	if (strpos($_GET['filenamenoext'], "_thumb") === false)
+		$filenoext_other = $_GET['year']."/".$_GET['month']."/".$_GET['day']."/".$_GET['filenamenoext']."_thumb";
+	else
+		$filenoext_other = $_GET['year']."/".$_GET['month']."/".$_GET['day']."/".str_replace("_thumb", "", $_GET['filenamenoext']);
 	if ($doc_root == "") {
-		$filenoext =        "$filesdirpath/$filenoext";
-		$filenoextnothumb = "$filesdirpath/$filenoextnothumb";
+		$filenoext = "$filesdirpath/$filenoext";
+		$filenoext_other = "$filesdirpath/$filenoext_other";
 	}
 	else {
-		$filenoext =        rtrim($doc_root,"\\/")."/".ltrim($filesdirpath,"\\/")."/$filenoext";
-		$filenoextnothumb = rtrim($doc_root,"\\/")."/".ltrim($filesdirpath,"\\/")."/$filenoextnothumb";
+		$filenoext = rtrim($doc_root,"\\/")."/".ltrim($filesdirpath,"\\/")."/$filenoext";
+		$filenoext_other = rtrim($doc_root,"\\/")."/".ltrim($filesdirpath,"\\/")."/$filenoext_other";
 	}
 	
-	// Rename to .recycled
+	// Rename filename to .recycled
 	if (is_file("$filenoext.mp4"))
 		strong_rename("$filenoext.mp4","$filenoext.mp4.recycled");
 	if (is_file("$filenoext.avi"))
@@ -57,8 +60,18 @@ if (isset($_GET['year']) && isset($_GET['month']) && isset($_GET['day']) && isse
 		strong_rename("$filenoext.gif","$filenoext.gif.recycled");
 	if (is_file("$filenoext.jpg"))
 		strong_rename("$filenoext.jpg","$filenoext.jpg.recycled");
-	if (is_file("$filenoextnothumb.jpg"))
-		strong_rename("$filenoextnothumb.jpg","$filenoextnothumb.jpg.recycled");
+	
+	// Rename other filename to .recycled
+	if (is_file("$filenoext_other.mp4"))
+		strong_rename("$filenoext_other.mp4","$filenoext_other.mp4.recycled");
+	if (is_file("$filenoext_other.avi"))
+		strong_rename("$filenoext_other.avi","$filenoext_other.avi.recycled");
+	if (is_file("$filenoext_other.swf"))
+		strong_rename("$filenoext_other.swf","$filenoext_other.swf.recycled");
+	if (is_file("$filenoext_other.gif"))
+		strong_rename("$filenoext_other.gif","$filenoext_other.gif.recycled");
+	if (is_file("$filenoext_other.jpg"))
+		strong_rename("$filenoext_other.jpg","$filenoext_other.jpg.recycled");
 	
 	// Reload page
 	if (isset($_GET['backuri'])) {
