7d451fab31aa44d57ee56ec3610b23eea929ca61
Oliver Pfister <oliver.pfister@contaware.com>
2016-11-18 22:45:30 +0000

    Final release 5.3.0
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1644 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -31,7 +31,7 @@
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #define MANUAL_ONLINE_PAGE								_T("http://www.contaware.com/freevimagermanual/")
 #endif
-#define APPVERSION										_T("5.3.0beta8")
+#define APPVERSION										_T("5.3.0")
 
 // Company
 #define MYCOMPANY										_T("Contaware")
