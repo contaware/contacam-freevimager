ae5c1c64004137e4547c19adb04be89801ebf01e
Oliver Pfister <oliver.pfister@contaware.com>
2018-08-30 20:56:27 +0000

    Reverted rev. 1935 as the lock cause was Windows, fixed by KB4343900 (we have now Apache 2.4.34 and PHP 5.6.37, both with OpenSSL 1.0.2o)
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@1969 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -30,7 +30,7 @@
 #define FAQ_ONLINE_PAGE									_T("http://www.contaware.com/freevimagerfaq/")
 #define MANUAL_ONLINE_PAGE								_T("http://www.contaware.com/freevimagermanual/")
 #endif
-#define APPVERSION										_T("9.0.0beta6")
+#define APPVERSION										_T("9.0.0beta7")
 
 // Company
 #define MYCOMPANY										_T("Contaware")
