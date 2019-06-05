ade025bc9d197d6c2faf3b323826b23d1fa3c9d9
Oliver Pfister <oliver.pfister@contaware.com>
2019-06-05 14:05:57 +0000

    Corrected Chinese web interface
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2171 15f0fa91-3113-114a-a8b1-9261d5763783

--- microapache/htdocs/languages/chinese.php
+++ microapache/htdocs/languages/chinese.php
@@ -1,14 +1,14 @@
 ﻿<?php
-define("今天","今天");
-define("天名字","'星期日','星期一','星期二','星期三','星期四','星期五','星期六'"); // Starting from sunday!
-define("在","在");
-define("天","天");
-define("天","天");
-define("一天前","一天前");
-define("几天前","几天前");
-define("没有文件","没有文件");
-define("没有文件","没有文件");
-define("网页","网页");
-define("图像重新加载","图像将重新加载");
-define("秒","秒");
-define("全日","全日");
+define("TODAY","今天");
+define("DAYNAMES","'星期日','星期一','星期二','星期三','星期四','星期五','星期六'"); // Starting from sunday!
+define("IN","在");
+define("DAY","天");
+define("DAYS","天");
+define("DAYAGO","一天前");
+define("DAYSAGO","几天前");
+define("NOFILE","没有文件");
+define("NOFILESFOR","没有文件");
+define("PAGES","页");
+define("IMAGERELOADIN","图像将重新加载");
+define("SECONDS","秒");
+define("FULLDAY","全日");
