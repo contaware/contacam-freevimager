c53f5c25f789c8e0c1ce44cad499f272cd2c3138
Oliver Pfister <oliver.pfister@contaware.com>
2019-09-05 11:55:33 +0000

    Added Swedish web interface language
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2231 15f0fa91-3113-114a-a8b1-9261d5763783

---
+++ microapache/htdocs/languages/swedish.php
@@ -0,0 +1,14 @@
+﻿<?php
+define("TODAY","idag");
+define("DAYNAMES","'Söndag','Måndag','Tisdag','Onsdag','Torsdag','Fredag','Lördag'"); // Starting from sunday!
+define("IN","om");
+define("DAY","dag");
+define("DAYS","dagar");
+define("DAYAGO","dag sedan");
+define("DAYSAGO","dagar sedan");
+define("NOFILE","Ingen fil");
+define("NOFILESFOR","Inga filer för");
+define("PAGES","sidor");
+define("IMAGERELOADIN","Bilden uppdateras om");
+define("SECONDS","sekunder");
+define("FULLDAY","Hel dag");
