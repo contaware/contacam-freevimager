62f1d1a19a3298d715ce59a81af930ea9d999dd6
Oliver Pfister <oliver.pfister@contaware.com>
2020-04-07 21:37:30 +0000

    With javascriptspecialchars() escape also $_SERVER['REQUEST_URI'] assigned to location.href
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2406 15f0fa91-3113-114a-a8b1-9261d5763783

--- microapache/htdocs/setusertz.php
+++ microapache/htdocs/setusertz.php
@@ -1,4 +1,5 @@
 <?php
+require_once( 'configuration.php' );
 if (!isset($_SESSION['timezonename']))
 {
 	// Set a default value to avoid warnings in PHP >= 5.3 if date.timezone is not set in php.ini
@@ -11,7 +12,7 @@ if (!isset($_SESSION['timezonename']))
 	ajax.open('GET', 'updatetimezonename.php?timezoneoffset=' + encodeURIComponent(timezoneoffset), false); // sync. call
 	ajax.send(null);
 	if (ajax.status == 200)
-		location.href = "<?php echo $_SERVER['REQUEST_URI']; ?>"; // reload page, timezonename session var is set
+		location.href = '<?php echo javascriptspecialchars($_SERVER['REQUEST_URI']); ?>'; // reload page, timezonename session var is set
 	//]]>
 	</script>
 	<?php
