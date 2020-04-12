c7bcb40b32399511752843415185bc5eea8be9cd
Oliver Pfister <oliver.pfister@contaware.com>
2020-04-12 20:07:29 +0000

    Changed doctype to html5 for last leftover
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2414 15f0fa91-3113-114a-a8b1-9261d5763783

--- microapache/htdocs/updatetimezonename.php
+++ microapache/htdocs/updatetimezonename.php
@@ -1,11 +1,10 @@
 <?php session_start(); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="author" content="Oliver Pfister" />
 <title>Update Timezone Name</title>
 </head>
