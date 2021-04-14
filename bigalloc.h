e674522fa9aff07cdd2a32e8f73ff5afc88bf6cd
Oliver Pfister <oliver.pfister@contaware.com>
2021-04-14 21:17:16 +0000

    Increased BIGALLOC_ALIGN to 64 to support AVX-512
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2629 15f0fa91-3113-114a-a8b1-9261d5763783

--- bigalloc.h
+++ bigalloc.h
@@ -8,7 +8,7 @@
 
 // - VirtualAlloc and VirtualFree leaks are not detected by the debugger,
 //   we better use the CRT heap functions
-// - VirtualAlloc's returned address is 64 KB aligned, 32 bytes alignment
+// - VirtualAlloc's returned address is 64 KB aligned, 64 bytes alignment
 //   is necessary for SIMD ops, _aligned_malloc handles that correctly
 // - Today's 32 bits apps suffer from memory space fragmentation and
 //   not from RAM shortage, ALLOC_GRANULARITY makes sure we allocate in
@@ -20,8 +20,8 @@
 //   to allocate in chunks of 64K to avoid fragmenting the standard heap
 // - From ffmpeg's mem.c: av_malloc(size) calls _aligned_malloc(size, ALIGN)
 //   with ALIGN equal to 32 if HAVE_AVX is defined, otherwise it is 16
-#define BIGALLOC_SAFETY		32U	// must be at least FF_INPUT_BUFFER_PADDING_SIZE
-#define BIGALLOC_ALIGN		32U	// 32 to support AVX or 16 for SSE
+#define BIGALLOC_SAFETY		64U	// must be at least FF_INPUT_BUFFER_PADDING_SIZE
+#define BIGALLOC_ALIGN		64U	// 64 to support AVX-512
 #define ALLOC_GRANULARITY(Size) (((Size) + (g_dwAllocationGranularity - 1U)) & ~(g_dwAllocationGranularity - 1U))
 #define BIGALLOC(Size) _aligned_malloc((size_t)ALLOC_GRANULARITY((Size)+BIGALLOC_SAFETY),BIGALLOC_ALIGN)
 #define BIGFREE(lpAddress) _aligned_free((void*)(lpAddress))
