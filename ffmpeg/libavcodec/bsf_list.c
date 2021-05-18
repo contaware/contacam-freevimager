6a0efac506262f5c8bce2c47c61686ebc402a9c5
Oliver Pfister <oliver.pfister@contaware.com>
2021-05-18 09:15:43 +0000

    Removing ffmpeg folders for toolchain update
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2648 15f0fa91-3113-114a-a8b1-9261d5763783

--- ffmpeg/libavcodec/bsf_list.c
+++
@@ -1,6 +0,0 @@
-static const AVBitStreamFilter * const bitstream_filters[] = {
-    &ff_aac_adtstoasc_bsf,
-    &ff_h264_mp4toannexb_bsf,
-    &ff_hevc_mp4toannexb_bsf,
-    &ff_null_bsf,
-    NULL };
