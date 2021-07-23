a079f4062f411e6e191a85dba8ba528f2a4d4e60
Oliver Pfister <oliver.pfister@contaware.com>
2021-07-23 13:12:36 +0000

    9.9.19beta4 release
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2685 15f0fa91-3113-114a-a8b1-9261d5763783

--- appconst.h
+++ appconst.h
@@ -36,7 +36,7 @@
 #define APPMUTEXNAME									_T("FreeVimagerAppMutex")
 #define MANUAL_ONLINE_PAGE								_T("https://www.contaware.com/freevimagermanual/")
 #endif
-#define APPVERSION										_T("9.9.19beta3")
+#define APPVERSION										_T("9.9.19beta4")
 
 // Company
 #define MYCOMPANY										_T("Contaware")
