6bca76fb736e11f09a9005e4d578c67fc04f8da6
Oliver Pfister <oliver.pfister@contaware.com>
2023-03-14 07:32:59 +0000

    Using https to download procdump.exe
    git-svn-id: file:///D/data/svnrepos/uimager/trunk@2829 15f0fa91-3113-114a-a8b1-9261d5763783

--- Setup/ContaCamDump.bat
+++ Setup/ContaCamDump.bat
@@ -11,7 +11,7 @@ if exist "%batchdir%procdump.exe" goto procdump_exec
 echo procdump.exe from Sysinternals is necessary, please save/move it to:
 echo "%batchdir%"
 pause
-start "" "http://live.sysinternals.com/procdump.exe"
+start "" "https://live.sysinternals.com/procdump.exe"
 :wait_procdump_download
 ping -n 2 127.0.0.1 >nul
 if not exist "%batchdir%procdump.exe" goto wait_procdump_download
