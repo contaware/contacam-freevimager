e133cdeb2cea8728422ed06fc71e562353dc0b35
Oliver Pfister <oliver.pfister@contaware.com>
2025-06-18 14:28:42 +0200

    Release 9.9.26

--- appconst.h
+++ appconst.h
@@ -35,7 +35,7 @@
 #define APPMUTEXNAME									_T("FreeVimagerAppMutex")
 #define MANUAL_ONLINE_PAGE								_T("https://www.contaware.com/freevimagermanual/")
 #endif
-#define APPVERSION										_T("9.9.26beta2")
+#define APPVERSION										_T("9.9.26")
 
 // Company
 #define MYCOMPANY										_T("Contaware")
